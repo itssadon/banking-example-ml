718467e7fdbb41459fb41f5b52fcdaf5525b5442
Peculiar <peculiarbabalola@gmail.com>
2026-03-25 15:24:46 +0100

    feat: uniform response body for the whole app. And some refactor

--- core/utils/ResponseHandler.js
+++ core/utils/ResponseHandler.js
@@ -4,41 +4,41 @@ class ResponseHandler {
         this.failure = 'Request is failed';
     }
 
-    successResponse(res, body) {
+    static successResponse(res, body) {
         res.status(200).send({
             status: this.success,
             body: body || {}
         });
     }
 
-    failureResponse(res, body) {
+    static failureResponse(res, body) {
         res.status(400).send({
             status: this.failure,
             body: body || {}
         });
     }
 
-    createdResponse(res, body) {
+    static createdResponse(res, body) {
         res.status(201).send({
             status: this.success,
             body: body || {}
         });
     }
 
-    notFoundResponse(res, body) {
+    static notFoundResponse(res, body) {
         res.status(404).send({
             status: this.failure,
             body: body || {}
         });
     }
 
-    internalServerErrorResponse(res, body) {
+    static internalServerErrorResponse(res, body) {
         res.status(500).send({
             status: this.failure,
             body: body || {}
         });
     }
-    errorResponse(res, error) {
+    static errorResponse(res, error) {
         res.status(500).send({
             status: this.failure,
             body: error || {}
--- domain/accountHolders/controller.js
+++ domain/accountHolders/controller.js
@@ -2,54 +2,32 @@ const AccountHolderService = require('../../services/AccountHolderService');
 const DepositService = require('../../services/DepositService');
 const accountHolderService = new AccountHolderService();
 const depositService = new DepositService();
-
+const ResponseHandler = require('../../core/utils/ResponseHandler')
 module.exports = {
 	async getAccountHolderByID(req, res) {
 		try {
       const { accountHolderID } = req.params;
       const result = await accountHolderService.getAccountHolderByID(accountHolderID);
-      return res.send({
-        status: 'success',
-        body: result || {}
-      });
+      ResponseHandler.successResponse(res, result);
     } catch (error) {
-      console.log('Error', error.message)
-      return res.status(400).send({
-        status: 'failure'
-      });
+      ResponseHandler.errorResponse(res, error);
     }
   },
 	async getAccountHolderDeposits(req, res) {
 		try {
       const { accountHolderID } = req.params;
       const result = await depositService.getDepositsByAccountHolderID(accountHolderID);
-
-      return res.send({
-        status: 'success',
-        body: result || {}
-      });
-
+      ResponseHandler.successResponse(res, result);
     } catch (error) {
-      console.log('Error', error.message)
-      return res.status(400).send({
-        status: 'failure'
-      });
+      ResponseHandler.errorResponse(res, error);
     }
   },
 	async createAccountHolderDeposit(req, res) {
 		try {
       const { accountHolderID } = await depositService.createDeposit(req.params.accountHolderID, req.query.amount);
-
-      return res.status(201).send({
-        status: 'success',
-        body: { accountHolderID }
-      });
-
+      ResponseHandler.createdResponse(res, { accountHolderID });
     } catch (error) {
-      console.log('Error', error.message)
-      return res.status(400).send({
-        status: 'failure'
-      });
+      ResponseHandler.errorResponse(res, error);
     }
   },
 };
