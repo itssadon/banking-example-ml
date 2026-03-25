d246ebfb7ff94ef5b9ec60b7a2b435f641c2f535
Peculiar <peculiarbabalola@gmail.com>
2026-03-25 15:26:27 +0100

    feat: withdrawal controller

--- domain/withdrawals/controller.js
+++ domain/withdrawals/controller.js
@@ -1,19 +1,37 @@
-const Deposit = require('../../infrastructure/models/Deposit')
-
+const ResponseHandler = require('../../core/utils/ResponseHandler')
+const WithdrawalService = require('../../services/WithdrawalService');
+const withdrawalService = new WithdrawalService();
 module.exports = {
 
 
-	async makeWithdrawals(req, res) {
+	async getWithdrawalByID(req, res) {
 		try{
 			const { accountHolderID } = req.params;
 			const { amount } = req.body;
-			
-			return res.send({
-				status: 'successs',
-				body: {}
-			})
+			const result = await withdrawalService.getWithdrawalByID(accountHolderID, amount);
+			ResponseHandler.successResponse(res, result);
 		}catch(error){
-
+			ResponseHandler.errorResponse(res, error);
+		}
+	},
+	async getWithdrawalByAccountHolderID(req, res) {
+		try{
+			const { accountHolderID } = req.params;
+			const result = await withdrawalService.getWithdrawalsByAccountHolderID(accountHolderID);
+			ResponseHandler.successResponse(res, result);
+		}catch(error){
+			ResponseHandler.errorResponse(res, error);
+		}
+	},
+	async makeWithdrawals(req, res) {
+		try{
+			const { accountHolderID } = req.params;
+			const { amount } = req.query;
+			const numAmount = Number(amount);
+			const result = await withdrawalService.makeWithdrawal(accountHolderID, numAmount);
+			ResponseHandler.successResponse(res, result);
+		}catch(error){
+			ResponseHandler.errorResponse(res, error);
 		}
 	}
 }
