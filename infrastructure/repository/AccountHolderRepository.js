a3c1d8d86547f7962f066d53afdd64b4e06f3cfa
Peculiar <peculiarbabalola@gmail.com>
2026-03-25 08:38:59 +0100

    refactor: removed every deposit related stuff from the accountHolder repository

--- infrastructure/repository/AccountHolderRepository.js
+++ infrastructure/repository/AccountHolderRepository.js
@@ -5,17 +5,6 @@ class AccountHolderRepository {
     async getAccountHolderByID(accountHolderID) {
         return await AccountHolder.query().findById(accountHolderID).withGraphFetched('deposits');
     }
-
-    async getAccountHolderDeposits(accountHolderID) {
-        return await Deposit.query().where({ accountHolderID });
-    }
-
-    async createAccountHolderDeposit(accountHolderID, amount) {
-        return await Deposit.query().insert({
-            accountHolderID,
-            amount
-        }).returning('*');
-    }
 }
 
 module.exports = AccountHolderRepository;
