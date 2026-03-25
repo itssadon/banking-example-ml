7130fd071410041ca72494c7737993d4c994487b
Peculiar <peculiarbabalola@gmail.com>
2026-03-25 15:20:39 +0100

    chore: includes the withdrawals router to the router so the app can see it

--- router.js
+++ router.js
@@ -3,5 +3,6 @@ const router = express.Router();
 
 router.use(require('./domain/accountHolders/router'));
 router.use(require('./domain/deposits/router'));
+router.use(require('./domain/withdrawals/router'));
 
 module.exports = router;
