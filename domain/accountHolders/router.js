41aa7d75f7a7f3155d918278c18196bce3ecb9a0
Peculiar <peculiarbabalola@gmail.com>
2026-03-25 15:25:28 +0100

    chore: seperation of concerns. Deposit and withdrawal features in their domains

--- domain/accountHolders/router.js
+++ domain/accountHolders/router.js
@@ -5,7 +5,5 @@ const controller = require('./controller');
 const PREFIX = 'account_holders';
 
 router.get(`/${PREFIX}/:accountHolderID`, controller.getAccountHolderByID);
-router.get(`/${PREFIX}/:accountHolderID/deposits`, controller.getAccountHolderDeposits);
-router.post(`/${PREFIX}/:accountHolderID/deposits`, controller.createAccountHolderDeposit);
 
 module.exports = router;
