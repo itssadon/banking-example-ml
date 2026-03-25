298d9af972b01644d37a218d3d6745fa753dff97
Peculiar <peculiarbabalola@gmail.com>
2026-03-25 12:26:15 +0100

    refactor: we replace the accountholderbalance with the balancequery repo dependency

--- services/AccountHolderService.js
+++ services/AccountHolderService.js
@@ -1,20 +1,18 @@
 const AccountHolderRepository = require('../infrastructure/repository/AccountHolderRepository');
+const BalanceQueryRepository = require('../infrastructure/repository/query/BalanceQueryRepository');
 
 class AccountHolderService {
     constructor() {
         this.accountHolderRepository = new AccountHolderRepository();
+        this.balanceQueryRepository = new BalanceQueryRepository();
     }
 
     async getAccountHolderByID(accountHolderID) {
         return await this.accountHolderRepository.getAccountHolderByID(accountHolderID);
     }
 
-    async getAccountHolderDeposits(accountHolderID) {
-        return await this.accountHolderRepository.getAccountHolderDeposits(accountHolderID);
-    }
-
-    async createAccountHolderDeposit(accountHolderID, amount) {
-        return await this.accountHolderRepository.createAccountHolderDeposit(accountHolderID, amount);
+    async getAccountHolderBalance(accountHolderID) {
+        return await this.balanceQueryRepository.getAccountHolderBalance(accountHolderID);
     }
 }
 
